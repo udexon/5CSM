377444ee6b779fdeb8a0982616c781d1088b9f59
udexon <udexon@users.noreply.github.com>
2018-11-29 11:58:18 +0800

    Add files via upload

---
+++ sd.s
@@ -0,0 +1,99 @@
+	.file	"sd.c"
+	.text
+	.globl	s_strcspn
+	.type	s_strcspn, @function
+s_strcspn:
+.LFB5:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	-16(%rbp), %rdx
+	movq	-8(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	strcspn@PLT
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE5:
+	.size	s_strcspn, .-s_strcspn
+	.section	.rodata
+.LC0:
+	.string	"   1234e5"
+.LC1:
+	.string	"A"
+.LC2:
+	.string	"   AAAAAA"
+.LC3:
+	.string	"eE"
+.LC4:
+	.string	"%d %c\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$288, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rax
+	movq	%rax, -280(%rbp)
+	leaq	.LC1(%rip), %rsi
+	leaq	.LC2(%rip), %rdi
+	call	s_strcspn
+	movl	%eax, -284(%rbp)
+	movq	-280(%rbp), %rax
+	leaq	.LC3(%rip), %rsi
+	movq	%rax, %rdi
+	call	s_strcspn
+	movl	%eax, -284(%rbp)
+	movl	-284(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-280(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	movsbl	%al, %edx
+	movl	-284(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC4(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	-284(%rbp), %eax
+	subl	$1, %eax
+	movslq	%eax, %rdx
+	movq	-280(%rbp), %rcx
+	leaq	-272(%rbp), %rax
+	movq	%rcx, %rsi
+	movq	%rax, %rdi
+	call	strncpy@PLT
+	leaq	-272(%rbp), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L5
+	call	__stack_chk_fail@PLT
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits
